c088f4737ee5306a925f9fd40ffcb428b5ff3f9f
PAO-create4259 <rabearivelopaolah31@gmail.com>
2025-10-22 09:11:23 +0300

    accueil

---
+++ pages/accueil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Budget des Citoyens 2025 - Accueil</title>
+<head>
+    <header>
+    <div class="logo">
+      <img src="logo-mef.png" alt="Logo MEF">
+      <h1>Budget des Citoyens 2025</h1>
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="accueil.html" class="active">Accueil</a></li>
+        <li><a href="perspectives.html">Perspectives</a></li>
+        <li><a href="recettes.html">Recettes</a></li>
+        <li><a href="depenses.html">Dépenses</a></li>
+        <li><a href="deficit.html">Déficit</a></li>
+        <li><a href="fiscalite.html">Fiscalité</a></li>
+        <li><a href="glossaire.html">Glossaire</a></li>
+      </ul>
+    </nav>
+  </header>
